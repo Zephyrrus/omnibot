46a9f89566ddad3eefd20b4b172822904b7b29a5
Zephy <krisztian1997@gmail.com>
2016-05-03 21:30:07 +0300

    More logging added to bot.

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
 // omnibot - a discord bot
 
+
 var discordie = require("discordie"),
-	fs = require("fs");
+	fs = require("fs"),
+	logger = require("winston");
 
 var commandeer = require("./lib/commandeer.js"),
 	commands = require("./lib/commands.js"),
 	webserver = require("./lib/web/webserver.js"),
 	mods = require("./lib/mods.js");
 
-console.log("[omni] Starting OmniBot v0.1");
+var config = require('./config/config.json');
+var auth = require('./config/auth.json');
+var package = require('./package.json');
 
-var config = JSON.parse(fs.readFileSync(__dirname + "/config/config.json", 'utf8'));
+/*----LOGGING----*/
+var dt = new Date();
+var t = (dt.getDate() + "_" + (dt.getMonth() + 1) + "_" + dt.getFullYear());
+var fname = './log/' + t + '.log';
+try {
+    fs.mkdirSync('./log');
+} catch (e) {
+
+}
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, { colorize: true });
+logger.add(logger.transports.File, {
+    level: 'debug',
+    filename: fname
+});
+logger.level = 'debug';
+////
+
+/*----Variables----*/
+config._ = {};
+config._.name = package.name;
+////
+
+logger.info(`[CORE] Starting ${config._.name}.`);
+logger.info(`[CORE] Version: ${package.version}.`);
+logger.info(`[CORE] Listening to: ${config.discord.listenTo}.`);
+
 var bot = new discordie();
 
 bot.connect({
-	token: config.token
+	token: auth.discord.token
 });
 
 bot.Dispatcher.on(discordie.Events.GATEWAY_READY, function(e) {
-	console.log("[omni] OmniBot started. Username: " + bot.User.username);
+	logger.info(`[CORE] ${config._.name} started. Username: ${bot.User.username}`);
 
 	global.InviteManager = bot.Invites;
 	global.bot_id = bot.User.id;
@@ -30,5 +60,10 @@ bot.Dispatcher.on(discordie.Events.GATEWAY_READY, function(e) {
 });
 
 bot.Dispatcher.on(discordie.Events.MESSAGE_CREATE, function(e) {
+	logger.verbose("[MESSAGE] (" + e.message.channel.name + ") [" + e.message.author.username + "]: " +
+									e.message.content.replace(/[^A-Za-z0-9.,\/#!$%\^&\*;:{}=\-_`~() ]/, '') +
+									(e.message.attachments[0] !== undefined ? "[attachments: " + e.message.attachments[0].url + " ]" : "")
+								);
+
 	commandeer.dispatch(e.message);
 });
